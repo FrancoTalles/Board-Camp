dc98c34b6220715936364050d05c765d6d0e646c
FrancoTalles <francotallesextreme@gmail.com>
2023-02-11 18:42:07 -0400

    Refatorando CRUD de Clientes v3

--- src/Controllers/customersController.js
+++ src/Controllers/customersController.js
@@ -23,7 +23,7 @@ export async function buscarClientePorId(req, res) {
       return res.sendStatus(404);
     }
 
-    res.send(customer.rows);
+    res.send(customer.rows[0]);
   } catch (error) {
     res.status(500).send(error.message);
   }
